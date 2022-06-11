c73c1a3f5e5f7b684a76e38713b5fcebd0d7d07a
Otto G <ottodevs@gmail.com>
2022-06-11 18:25:04 +0200

    add initial generative config

---
+++ scripts/art-gen/layerConfig.js
@@ -0,0 +1,19 @@
+const layerConfigurations = [
+    {
+        growEditionSizeTo: 10,
+        layersOrder: [
+            { name: 'Background Color' }, // 5
+            { name: 'Background Item' }, // 20
+            { name: 'Silhouette' },
+            { name: 'Body' }, // 10
+            { name: 'Head' }, // 10
+            { name: 'Face' }, // 10
+            { name: 'Skin Color' },
+            { name: 'Spots' },
+            { name: 'Item' }, // 15-20
+            // { name: 'Extintion Risk' },
+            // { name: 'Species' }, // 7 + 1 G + 1D
+            // { name: 'Original Pictures' }, // Array
+        ],
+    },
+];
